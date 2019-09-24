01c29813e5651c52b8b1b0c251a5cdb7d34b33cd
ffreemt <yucongo+fmt@gmail.com>
2019-09-24 15:55:15 +0800

    minor update

--- update_repo/update_repo.py
+++ update_repo/update_repo.py
@@ -19,27 +19,12 @@ def update_repo(url=THISREPO, destdir='/content'):
     >>> update_repo(repo_https_git_address)
     '''
 
-    # dirname = Path(url).stem
-    repo_dirname = Path(url).stem
-    # chdir('/content')
     if Path(destdir).exists():
         chdir(destdir)
     else:
         chdir(Path().home())
-    if not Path(repo_dirname).exists():
-        print('git clone %s...' % url)
-        # os.system('git clone %s' % url)
-        proc = sp.Popen(
-            shlex.split('git clone %s' % url),
-            stdout=sp.PIPE,
-            stderr=sp.PIPE,
-        )
-        out, err = proc.communicate()
-        if err:
-            print('>> %s, %s' % (err.decode(), out.decode()))
-        else:
-            print('git clone...%s' % out.decode())
-    else:
+
+    if Path(repo_dirname).exists():
         chdir(repo_dirname)
         print('git pull %s...' % url)
         # os.system('git pull')
@@ -52,5 +37,19 @@ def update_repo(url=THISREPO, destdir='/content'):
         if err:
             print('>> %s' % err.decode())
         print('git pull ...%s' % out.decode())
+    else:
+        print('git clone %s...' % url)
+        # os.system('git clone %s' % url)
+        proc = sp.Popen(
+            shlex.split('git clone %s' % url),
+            stdout=sp.PIPE,
+            stderr=sp.PIPE,
+        )
+        out, err = proc.communicate()
+        if err:
+            print('>> %s, %s' % (err.decode(), out.decode()))
+        else:
+            print('git clone...%s' % out.decode())
+
     chdir('%s/%s' % repo_dirname)
     print(' Now in %s/%s\n' % (destdir, repo_dirname))
