59ecc90417ddd515eca8f01b6a2afc362ca8d4ad
freemt <yucongo+fmt@gmail.com>
2020-12-18 09:45:12 +0800

    update version 0.0.8

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# update-repo [![Codacy Badge](https://app.codacy.com/project/badge/Grade/3c2abd87173b4b60ade389fbd4ef8d86)](https://www.codacy.com/gh/ffreemt/update-repo/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ffreemt/update-repo&amp;utm_campaign=Badge_Grade)
+# update-repo [![Codacy Badge](https://app.codacy.com/project/badge/Grade/3c2abd87173b4b60ade389fbd4ef8d86)](https://www.codacy.com/gh/ffreemt/update-repo/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ffreemt/update-repo&amp;utm_campaign=Badge_Grade)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![PyPI version](https://badge.fury.io/py/update-repo.svg)](https://badge.fury.io/py/update-repo)
 
 Update git repo: clone if the repo does not exist; pull if the repo already exist.
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-r'''Clone/pull a git repo.'''
+r"""Clone/pull a git repo."""
 # pylint: disable=invalid-name
 from pathlib import Path
 import re
@@ -7,15 +7,21 @@ from setuptools import setup, find_packages  # type: ignore
 
 name = "update-repo"
 # description = ' '.join(name.split('-'))
-description = 'clone/pull a repo'
-dir_name, = find_packages()
+description = "clone/pull a repo"
+(dir_name,) = find_packages()
 
-version, = re.findall(r"\n__version__\W*=\W*'([^']+)'", open('%s/__init__.py' % dir_name).read())
+_ = Path(f"{dir_name}/__init__.py").read_text()
+(version,) = re.findall(r"\n__version__\W*([\d.]+)", _)
 
-README_rst = '%s/README.md' % Path(__file__).parent
-long_description = open(README_rst, encoding='utf-8').read() if Path(README_rst).exists() else ''
-if Path('requirements.txt').exists():
-    install_requires = [elm.split()[0] for elm in Path('requirements.txt').read_text(encoding='utf-8').split('\n')]
+README_rst = "%s/README.md" % Path(__file__).parent
+long_description = (
+    open(README_rst, encoding="utf-8").read() if Path(README_rst).exists() else ""
+)
+if Path("requirements.txt").exists():
+    install_requires = [
+        elm.split()[0]
+        for elm in Path("requirements.txt").read_text(encoding="utf-8").split("\n")
+    ]
 else:
     install_requires = []
 
@@ -25,19 +31,19 @@ setup(
     version=version,
     description=description,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords=['git', 'colab'],
+    long_description_content_type="text/markdown",
+    keywords=["git", "colab"],
     author="mikeee",
-    url='http://github.com/ffreemt/%s' % name,
-    download_url='https://github.com/ffreemt/%s/archive/v_%s.tar.gz' % (name, version),
+    url="http://github.com/ffreemt/%s" % name,
+    download_url="https://github.com/ffreemt/%s/archive/v_%s.tar.gz" % (name, version),
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
     ],
-    license='MIT License',
+    license="MIT License",
 )
